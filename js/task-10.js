5c9647400b121dfd311ea7ad209e2f2b9fde6644
Iryusha <Sherbinina_Irina@ukr.net>
2022-01-17 20:52:13 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,6 @@
+const boxItem = document.querySelector('#boxes')
+const controlItemInput = document.querySelector('#controls input')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
